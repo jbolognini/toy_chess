d6fd866f14d5bff655caa4745b7e7aa75029d064
Jesse Bolognini <jbologna@gmail.com>
2026-02-06 22:26:26 -0500

    working on cache update

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = "toy_chess";
+const CACHE = "toy_chess_v3"; // bump when you want to force-update
+
 const FILES = [
   "./",
   "./index.html",
@@ -9,17 +10,30 @@ const FILES = [
   "./input.js",
   "./engine.js",
   "./engine.worker.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./assets/knight.png"
 ];
 
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE)
+      .then((c) => c.addAll(FILES))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k))))
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then((r) => r || fetch(e.request))
   );
 });
